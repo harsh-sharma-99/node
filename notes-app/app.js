e568bf87f0a97a54c76b981c49b3de523fc11a73
Harsh Sharma <sharmaharsh9944@gmail.com>
2023-01-23 09:12:40 +0530

    added files reuire

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,16 @@
-const fs  = require('fs');
+require('./notes.js');
 
+console.log("=======notesApp=======")
+
+
+
+
+
+
+
+
+
+
+// const fs  = require('fs');
 // fs.writeFileSync('notes.txt', "I am hasrh sharma");
-fs.appendFileSync('notes.txt', "Haha I won")
+// fs.appendFileSync('notes.txt', "Haha I won")
